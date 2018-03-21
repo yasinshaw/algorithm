eccc0eb5a9b0f2de811df711f99e6bae1864eefd
yasinshaw <yasinshaw@gmail.com>
2018-03-21 18:07:27 +0800

    索引堆

---
+++ src/main/java/heap/MyIndexHeap.java
@@ -0,0 +1,122 @@
+package heap;
+
+import java.util.Comparator;
+
+public class MyIndexHeap<T> implements IHeap<T> {
+    T[] datas; // 存放数据的数组 datas[1..n]
+    int[] indexes; // 索引数组
+    private int count = 0; // 计数
+    private Comparator<T> comparator; // 比较器
+
+    // 使用空数组构造，容量为capacity
+    public MyIndexHeap(int capacity, Comparator<T> comparator) {
+        this.datas = (T[]) new Object[capacity + 1];
+        // 为索引开辟空间
+        this.indexes = new int[capacity + 1];
+        this.comparator = comparator;
+    }
+
+    // 使用已给的数组构造，容量为数组长度
+    public MyIndexHeap(T[] datas, Comparator<T> comparator) {
+        this.count = datas.length;
+        this.datas = (T[]) new Object[count + 1];
+        this.indexes = new int[count + 1];
+        for (int i = 0; i < count + 1; i++) {
+            indexes[i] = i;
+        }
+        this.comparator = comparator;
+        System.arraycopy(datas, 0, this.datas, 1, count);
+        // 对前半部分逆序下沉，构造堆
+        for (int k = count / 2; k >= 1; k--)
+            sink(k);
+        // 对最后一层，也就是后半部分，不断交换到堆顶，再下沉
+        int k = count;
+    }
+
+    @Override
+    public int size() {
+        return count;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public void add(T data) {
+        if (count == datas.length - 1)
+            throw new RuntimeException("堆已满");
+        datas[indexes[++count]] = data;
+        swim(count);
+    }
+
+    @Override
+    public T pop() {
+        return remove(1);
+    }
+
+    @Override
+    public T remove(int i) {
+        if (isEmpty())
+            throw new RuntimeException("堆为空");
+        if (i < 1 || i > count)
+            throw new RuntimeException("下标非法");
+        T res = datas[indexes[i]];
+        datas[indexes[i]] = datas[indexes[count]];
+        datas[indexes[count--]] = null; // 防止对象游离
+        sink(i);
+        return res;
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty())
+            throw new RuntimeException("堆为空");
+        return datas[indexes[1]];
+    }
+
+    /**
+     * 比较两个数，看谁上浮
+     * @param i 第一个数
+     * @param j 第二个数
+     * @return true 为i上浮， false 为j上浮
+     */
+    private boolean needSwim(int i, int j) {
+        return comparator.compare(datas[indexes[i]], datas[indexes[j]]) < 0;
+    }
+
+    private void exchange(int i, int j) {
+        int temp = indexes[i];
+        indexes[i] = indexes[j];
+        indexes[j] = temp;
+    }
+
+    /**
+     * 上浮
+     * @param k 开始上浮的位置
+     */
+    private void swim(int k) {
+        while (k > 1 && needSwim(k, k / 2)) {
+            exchange(k, k / 2);
+            k /= 2;
+        }
+    }
+
+    /**
+     * 下沉
+     * @param k 开始下沉的位置
+     */
+    private void sink(int k) {
+        while (2 * k <= count) {
+            int j = 2 * k;
+            // 判断是否超出范围，再判断j 和 j + 1 那个更需要上浮
+            if (j < count && needSwim(j + 1, j))
+                j++; // 跟需要上浮的那个换
+            if (needSwim(j, k))
+                exchange(k, j);
+            else break;
+            k = j;
+        }
+    }
+}
---
+++ src/test/java/heap/MyIndexHeapTest.java
@@ -0,0 +1,64 @@
+package heap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyIndexHeapTest {
+
+    IHeap<Integer> heap;
+
+    @Before
+    public void setUp() throws Exception {
+        Integer[] arr = { 4,7, 3, 2, 9};
+        heap = new MyIndexHeap<Integer>(arr, (a, b) -> a - b);
+    }
+
+    @Test
+    public void constructorTest() throws Exception {
+        heap = new MyHeap<Integer>(10, (a, b) -> a - b);
+        heap.add(5);
+        heap.add(8);
+        heap.add(3);
+        heap.add(11);
+        assertTrue(heap.size() == 4);
+        assertTrue(heap.peek() == 3);
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+        assertFalse(heap.isEmpty());
+        for (int i = 0; i < 5; i++)
+            heap.pop();
+        assertTrue(heap.isEmpty());
+    }
+
+    @Test
+    public void add() throws Exception {
+        heap.pop();
+        heap.add(1);
+        assertTrue(heap.peek() == 1);
+    }
+
+    @Test
+    public void pop() throws Exception {
+        assertTrue(heap.pop() == 2);
+    }
+
+    @Test
+    public void remove() throws Exception {
+        assertTrue(heap.remove(1) == 2);
+    }
+
+    @Test
+    public void peek() throws Exception {
+        assertTrue(heap.peek() == 2);
+    }
+
+    @Test
+    public void size() throws Exception {
+        assertTrue(heap.size() == 5);
+    }
+
+}
