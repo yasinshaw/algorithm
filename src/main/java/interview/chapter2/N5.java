78aa1f615f23db388264da34faa14eaaaf21f62a
yasinshaw <yasinshaw@gmail.com>
2019-12-04 06:54:21 +0800

    implement interview chapter 2 N5

---
+++ src/main/java/interview/chapter2/N5.java
@@ -0,0 +1,34 @@
+package interview.chapter2;
+
+public class N5 {
+    public Node reversePart(Node head, int from, int to) {
+        int len = 0;
+        Node cur = head;
+        Node fPre = null;
+        Node tPos = null;
+        while (cur != null) {
+            len++;
+            fPre = len == from - 1 ? cur : fPre;
+            tPos = len == to + 1 ? cur : tPos;
+            cur = cur.next;
+        }
+        if (from > to || from < 1 || to > len) {
+            return head;
+        }
+        Node pre = fPre == null ? head : fPre.next;
+        cur = pre.next;
+        pre.next = tPos;
+        Node next;
+        while (cur != tPos) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        if (fPre != null) {
+            fPre.next = pre;
+            return head;
+        }
+        return pre;
+    }
+}
